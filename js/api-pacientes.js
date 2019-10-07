955c251cc4bd0d2d8918d03a9efb6cc0f99a73c2
GabrielNP <gabrielnp.2010@gmail.com>
2019-10-07 00:30:48 -0300

    teste

--- js/api-pacientes.js
+++ js/api-pacientes.js
@@ -9,11 +9,62 @@ botaoIntegrar.addEventListener("click", function(){
     var erroAjax = document.querySelector("#erro-ajax");
         if(xhr.status == 200){
             var resposta = xhr.responseText;
+            //console.log(resposta);
             var pacientes = JSON.parse(resposta);
+            
+            var count = Object.keys(pacientes).length;
+            //console.log(count);            
             erroAjax.classList.add("invisivel");
-            pacientes.forEach(function(paciente){
-                adicionaPacienteNaTabela(paciente);
-            });
+
+            /*for (x=0;count;x++) {
+                console.log(pacientes[x]['peso']);
+            }
+
+            for (key in pacientes) {
+                if (pacientes.hasOwnProperty(key)) {
+                    var value = pacientes[key];
+                    console.log(value);
+                }
+            }*/
+
+            var buz = {
+                firstName: "John",
+                lastName: "Smith",
+                isAlive: true,
+                age: 27,
+                address: {
+                  streetAddress: "21 2nd Street",
+                  city: "New York",
+                  state: "NY",
+                  postalCode: "10021-3100"
+                }}
+            ;
+              console.log(buz['address']['streetAddress']);
+              for (var name in buz) {
+                if (buz.hasOwnProperty(name)) {
+                  console.log( name + ": \t" + buz[name]);
+
+                  if (name == "address") {
+                      for (var obj in name) {
+                          if(name.hasOwnProperty(obj))
+                            console.log(obj + "\t" + name[obj]);
+                      }
+                  }
+                }
+                else {
+                  console.log(name); // toString or something else
+                }
+              }
+
+          /*  pacientes.forEach(function(paciente){
+                var nome = (paciente['nome']);
+                var peso = (paciente['peso']);
+                var altura = (paciente['altura']);
+                var gordura = (paciente['gordura']);
+                var imc = (paciente['imc']);
+                
+                adicionaPacienteNaTabela(nome,peso,altura,gordura,imc);
+            });*/
         }else{
             console.log("Erro",xhr.status);
             console.log(xhr.response);
